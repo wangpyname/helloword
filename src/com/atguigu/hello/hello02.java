5fb2c4ef0760ef063707bbf39f408f4a04ed0f46
wangpyname <2579026556@qq.com>
2016-08-26 07:21:19 +0800

    update hello02

--- src/com/atguigu/hello/hello02.java
+++ src/com/atguigu/hello/hello02.java
@@ -2,6 +2,11 @@ package com.atguigu.hello;
 
 public class hello02 {
   
-  system out print("p2123123")
+	  public static void main(String[] args) {
+	  System.out.println("p2,01");
+	  System.out.println("p2,02");
+	  
+  }
+	  
 
 }
